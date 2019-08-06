6334232fee6d031fbcabbf4aa67c366d1cc5323d
Santiago Soler <santiago.r.soler@gmail.com>
2019-08-06 10:59:35 -0300

    Add files automatically created by versioneer

--- modelling_earth/__init__.py
+++ modelling_earth/__init__.py
@@ -1,10 +1,11 @@
 # pylint: disable=missing-docstring
 # Import functions/classes to make the public API
-# from . import version
+from ._version import get_versions
 from .temperature import (
     add_subducting_slab, empty_temperature_grid, litho_astheno_temperatures,
 )
 
 
 # Get the version number through versioneer
-# __version__ = version.full_version
+__version__ = get_versions()['version']
+del get_versions
