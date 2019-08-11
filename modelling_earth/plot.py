46efc96fbe92b6de3608d3bac9c9f83ceeb5afd7
Agustina Pesce <pesce.agustina@gmail.com>
2019-08-11 15:15:44 -0300

    Fix error inthe fuction to plot the velocuty

--- modelling_earth/plot.py
+++ modelling_earth/plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import xarray as xr
 import numpy as np
 
-def quiver_velocity_2d(dataset, time_step, fil=4 ):
+def quiver_velocity_2d(dataset, time, fil=4 ):
     '''
     Plot a 2D velocity field of arrows over the temperature. 
         
@@ -12,14 +12,12 @@ def quiver_velocity_2d(dataset, time_step, fil=4 ):
     dataset : :class:`xarray.Dataset`
         Array containing the velocity data.
     all_time : 
-    step : float
-        Step of time how make the plot.
+    time : float
+        Time to generate the plot.
     fil : float
         Use to filter the number of arrows to plot.
     '''
     f = fil
-    # Calculate the time link to the time_step
-    time = dataset.time.values[dataset.step.values == time_step][0]
     # Extract the coordinate to grid
     x = dataset.x.values
     z = dataset.z.values
