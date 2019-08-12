4cb5730abe42b47357761392dc6c8419153072dc
Agustina Pesce <pesce.agustina@gmail.com>
2019-08-11 22:58:23 -0300

    Fix errors.

--- modelling_earth/plot.py
+++ modelling_earth/plot.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import xarray as xr
 import numpy as np
 
-def quiver_velocity_2d(dataset, time, fil=4, temper_min,            
-                       temper_max, temper_interval=10):
+def quiver_velocity_2d(dataset, time, fil=4, temper_levels=None):
     '''
     Plot a 2D velocity field of arrows over the temperature. 
         
@@ -17,21 +16,16 @@ def quiver_velocity_2d(dataset, time, fil=4, temper_min,
         Time to generate the plot.
     fil : float
         Use to filter the number of arrows to plot.
-    temper_min : float
-        Minimum temperature value to define the levels in the temperature plot.
-    temper_max :  float
-        Maximum temperature value to define the levels in the temperature plot.
-    temper_interval : float
-        Temperature interval to calculate the temperature level to plot
+    temper_levels : array
+        Array with the levels values to plot the temperature.
     '''
     f = fil
     # Extract the coordinate to grid
     xx, zz = np.meshgrid(dataset.x.values, dataset.z.values)
     # Plot the temperature
     plot_args = {'x': 'x', 'y': 'z'}
-    temper_nevels = np.arange(temper_max, temper_max, temper_interval)
     dataset.temperature.sel(time=time,y=0).plot.pcolormesh(**plot_args, 
-                            levels=temper_nevels)
+                            levels=temper_levels)
     # Plot the velocity
     plt.quiver(xx[::f, ::f], zz[::f, ::f],
                dataset.velocity_x.sel(time=time, y=0).values[::f, ::f].T,
@@ -39,8 +33,7 @@ def quiver_velocity_2d(dataset, time, fil=4, temper_min,
     plt.title('run/Time: %8.2f Ma'%time)
 
 
-def save_velocity_2d(dataset, save_path, fil=4, temper_min,            
-                     temper_max, temper_interval=10):
+def save_velocity_2d(dataset, save_path, fil=4, temper_levels=None):
     '''
     Plot and save the 2D velocity field of arrows over the temperature field 
     for each time.     
@@ -52,18 +45,14 @@ def save_velocity_2d(dataset, save_path, fil=4, temper_min,
         Path to the directory to save the plots.
     fil : float
         Use to filter the number of arrows to plot.
-    temper_min : float
-        Minimum temperature value to define the levels in the temperature plot.
-    temper_max : float
-        Maximum temperature value to define the levels in the temperature plot.
-    temper_interval : float
-        Temperature interval to calculate the temperature level to plot    
+    temper_levels : array
+        Array with the levels values to plot the temperature.
     '''
     for i in dataset.time:
-        # Plot the velocuty and the themperature
+        # Plot the velocity and the temperature
         plt.figure(figsize=(10 * 2, 2.5 * 2))
-        quiver_velocity_2d(dataset, time=i, fil=4)
-        # Calcule the step to create the nameof the plot
+        quiver_velocity_2d(dataset, time=i, fil=4, temper_levels=temper_levels)
+        # Calculate the step to create the name of the plot
         k = dataset.step.values[dataset.time.values == i.values][0]
         plt.title('run/Time: %8.2f Ma'%i.values)
         filename = 'velocity' + str(k).zfill(5) +".png"
@@ -71,5 +60,7 @@ def save_velocity_2d(dataset, save_path, fil=4, temper_min,
         plt.savefig(os.path.join(save_path, filename))
         plt.close()
     print('Velocity plot save in:', save_path)
+
+
 
 
