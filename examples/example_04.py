d8e7ad13eb19073562e4815db6e1766f21bfbb7f
Agustina Pesce <pesce.agustina@gmail.com>
2019-08-13 16:35:33 -0300

    Fix the example 4

--- examples/example_04.py
+++ examples/example_04.py
@@ -8,16 +8,20 @@ import matplotlib.pyplot as plt
 script_path = os.path.dirname(os.path.abspath(__file__))
 md3d_output_path = os.path.join(script_path, 'run')
 
-list_frame = me.swarm_read(md3d_output_path, rank=3)
+# Read the particles
+list_frame = me.read_swarm(md3d_output_path)
+# Take the particle position for step=0
 frame_0 = list_frame[0]
-#print(frame_0.x)
 
 # Read the MD3D output files
 data = me.read_md3d_data(md3d_output_path)
 
-# Plot the particle position over the temperature
+# Plot the particle position over the temperature for step=0
 plt.figure(figsize=(10 * 2, 2.5 * 2))
 plot_args = {"x": "x", "y": "z"}
 data.temperature.sel(time=0, y=0).plot.pcolormesh(**plot_args)
-plt.plot(frame_0.x, frame_0.z,"c.", color="black", markersize=0.3)
+plt.plot(frame_0.x[frame_0.cc0%10==0],
+         frame_0.z[frame_0.cc0%10==0],"c.",
+         color="black", markersize=0.3
+         ) # With [frame_0.cc0%10==0] filter the number of particles to plot
 plt.show()
