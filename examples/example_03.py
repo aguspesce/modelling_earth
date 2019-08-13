f8774d47f03864ab156f5e5d8d01f2ccf153e3a9
Santiago Soler <santiago.r.soler@gmail.com>
2019-08-13 10:54:09 -0300

    Add profile filter dict on plot example

--- examples/example_03.py
+++ examples/example_03.py
@@ -13,19 +13,18 @@ md3d_output_path = os.path.join(script_path, "run")
 # Read the MD3D output files
 dataset = me.read_md3d_data(md3d_output_path)
 
-# Filter the dataset for y=0
-dataset = dataset.sel(y=0)
+# Create filter to get only the y=0 profile of the dataset
+filter_profile = {"y": 0}
 
 # Plot single frame (for t=0)
 fig, ax = plt.subplots()
-me.plot_scalar_2d(dataset.temperature.sel(time=0), ax=ax)
-me.plot_velocity_2d(dataset.sel(time=0), ax=ax, slice_grid=(4, 3))
+me.plot_scalar_2d(dataset.temperature.sel(time=0, **filter_profile), ax=ax)
+me.plot_velocity_2d(dataset.sel(time=0, **filter_profile), ax=ax, slice_grid=(4, 3))
 ax.set_aspect("equal")
 plt.show()
-
 
 # Plot all temperatures and velocities for every time and save the figures
 figs_dir = os.path.join(script_path, "figures")
 if not os.path.isdir(figs_dir):
     os.mkdir(figs_dir)
-me.save_plots_2d(dataset, figs_dir, scalar_to_plot="temperature")
+me.save_plots_2d(dataset.sel(**filter_profile), figs_dir, scalar_to_plot="temperature")
