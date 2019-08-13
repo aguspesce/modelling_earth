5fe1939822fbd3de7c43af560afd57fd4f94bd34
Santiago Soler <santiago.r.soler@gmail.com>
2019-08-13 10:56:32 -0300

    Run black

--- modelling_earth/__init__.py
+++ modelling_earth/__init__.py
@@ -8,7 +8,11 @@ from .temperature import (
 )
 from .io import read_md3d_data
 from .plot import (
-    plot_velocity_2d, plot_scalar_2d, save_velocity_2d, plot_data_2d, save_plots_2d
+    plot_velocity_2d,
+    plot_scalar_2d,
+    save_velocity_2d,
+    plot_data_2d,
+    save_plots_2d,
 )
 
 
