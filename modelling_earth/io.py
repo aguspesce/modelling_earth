59485ce559e412ce9a519c7569462adb5755fe9b
Agustina Pesce <pesce.agustina@gmail.com>
2019-08-13 14:29:27 -0300

    Change list for array.

--- modelling_earth/io.py
+++ modelling_earth/io.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import xarray as xr
+import pandas as pd
 
 PARAMETERS_FILE = "param_1.5.3.txt"
 SCALARS = {
@@ -277,36 +278,34 @@ def read_swarm(path, rank=4):
         Number of processors used in the simulation.
     Returns: 
     -------
-    list_frame
- : list
-        List of `xarray.Dataset` which contains the coordinate `x`, `y` and `z`
-       for each time step.
+    list_position : list
+        List of `pandas.DataFrame` which contains the coordinate `x`, `y` and 
+       `z` and the flag `c_0` for each time step.
     """
-    x, y, z, cc, cc0 = [], [], [], [], []
-    list_frame = []
+    # Define variable and parameters
+    list_position = []
+    x = []
     parameters = _read_parameters(path)
     print_step = parameters["print_step"]
     max_steps = parameters["stepMAX"]
-    # Red the data
+    #max_steps = 50
+    # Red the data 
     for step in range(0, max_steps + print_step, print_step):
-        # Read data
-        for rank in range(rank):
-            filename = "step_{}-rank{}.txt".format(step, rank)
+        print(step)
+        x, y, z, cc0 = np.array([]), np.array([]), np.array([]), np.array([])
+        for rank_i in range(rank):
+            print(rank_i)
+            filename = "step_{}-rank{}.txt".format(step, rank_i)
             x1, y1, z1, c0, c1, c2, c3, c4 = np.loadtxt(
                 os.path.join(path, filename), unpack=True
             )
-            cc0 = np.append(cc0, c0)
-            cc = np.append(cc, c1)
-            x = np.append(x, x1)
-            y = np.append(y, y1)
-            z = np.append(z, z1)
-            # Filter the number of points
-            xx = x[cc0 % 10 == 0]
-            yy = y[cc0 % 10 == 0]
-            zz = z[cc0 % 10 == 0]
-            # Create a data frame 
-            data = {"x": xx, "y": yy, "z": zz}
+            cc0 =  np.hstack((cc0, c0))
+            x =  np.hstack((x, x1))
+            y =  np.hstack((y, y1))
+            z =  np.hstack((z, z1))
+            # Create a data frame
+            data = {'x': x, 'y': y, 'z': z}
             frame = pd.DataFrame(data=data)
         # Create a list with the frame
-        list_frame.append(frame)
-    return list_frame
+        list_position.append(frame)
+    return list_position
