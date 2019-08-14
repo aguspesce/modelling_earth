eb6c5ae681c03cbf449e4cf0abbe56759f25855e
Agustina Pesce <pesce.agustina@gmail.com>
2019-08-14 11:10:03 -0300

    Add a description in the example.

--- examples/example_04.py
+++ examples/example_04.py
@@ -1,3 +1,7 @@
+"""
+Read MD3D output files and plot temperature and position of the particle.
+"""
+
 import os
 import numpy as np
 import xarray as xr
@@ -6,7 +10,7 @@ import matplotlib.pyplot as plt
 
 # Get path to the MD3D output directory
 script_path = os.path.dirname(os.path.abspath(__file__))
-md3d_output_path = os.path.join(script_path, 'run')
+md3d_output_path = os.path.join(script_path, "run")
 
 # Read the particles
 list_frame = me.read_swarm(md3d_output_path)
@@ -20,8 +24,11 @@ data = me.read_md3d_data(md3d_output_path)
 plt.figure(figsize=(10 * 2, 2.5 * 2))
 plot_args = {"x": "x", "y": "z"}
 data.temperature.sel(time=0, y=0).plot.pcolormesh(**plot_args)
-plt.plot(frame_0.x[frame_0.cc0%10==0],
-         frame_0.z[frame_0.cc0%10==0],"c.",
-         color="black", markersize=0.3
-         ) # With [frame_0.cc0%10==0] filter the number of particles to plot
+plt.plot(
+    frame_0.x[frame_0.cc0 % 10 == 0],
+    frame_0.z[frame_0.cc0 % 10 == 0],
+    "c.",
+    color="black",
+    markersize=0.3,
+)  # With [frame_0.cc0%10==0] filter the number of particles to plot
 plt.show()
