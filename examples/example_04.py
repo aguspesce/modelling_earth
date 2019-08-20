b8b8ae554f68ac0a9f19cf9993b5988009aa581d
Agustina Pesce <pesce.agustina@gmail.com>
2019-08-20 14:26:46 -0300

    Fix the example_04

--- examples/example_04.py
+++ examples/example_04.py
@@ -12,10 +12,11 @@ output_path = os.path.join(script_path, 'output')
 
 # Read and save the particles position
 position, time = me.read_swarm(path=md3d_output_path, save=True, save_path=output_path)
-position, time = me.read_swarm(md3d_output_path)
 # Take the particle position for time=0 linked to index 0
 position_0 = position[0]
-time_0 = time[0]
+# Filter the number of particles
+position_fil = position_0[position_0.cc0%10==0]
+
 
 # Read the MD3D output files
 data = me.read_md3d_data(md3d_output_path)
@@ -23,8 +24,5 @@ data = me.read_md3d_data(md3d_output_path)
 # Plot the particle position over the temperature for step=0
 fig, ax = plt.subplots()
 me.plot_scalar_2d(data.temperature.sel(time=time_0, y=0), ax=ax)
-# Filter the number of particles to plot
-fewer_particles = position_0.sample(2500, random_state=0)
-ax.scatter(fewer_particles.x, fewer_particles.z, c='black', s=0.3)
+plt.plot(position_fil.x, position_fil.z,  "c.", color="black", markersize=0.3)
 plt.show()
-
