53aa033237d6cb5eafb284b89aacece7c0197866
Agustina Pesce <pesce.agustina@gmail.com>
2019-08-21 15:37:17 -0300

    Improve the example

--- examples/read_swarm.py
+++ examples/read_swarm.py
@@ -12,10 +12,13 @@ output_path = os.path.join(script_path, "output")
 
 # Read the particles position
 swarm = me.read_swarm(path=md3d_output_path)
+
 # Take the particle position for time=0 linked to index 0
-position_0 = swarm['positions'][0]
-# Filter the number of particles
-position_fil = position_0[position_0.cc0 % 10 == 0]
+positions = swarm['positions']
+positions = positions[0]
+
+# Filter the number of particles to take only 2 particle per cell
+positions = positions[positions.cc0 < 2]
 
 # Read the MD3D output files
 data = me.read_md3d_data(md3d_output_path)
@@ -23,5 +26,5 @@ data = me.read_md3d_data(md3d_output_path)
 # Plot the particle position over the temperature for step=0
 fig, ax = plt.subplots()
 me.plot_scalar_2d(data.temperature.sel(time=0, y=0), ax=ax)
-plt.plot(position_fil.x, position_fil.z, "c.", color="black", markersize=0.3)
+plt.plot(positions.x, positions.z, "c.", color="black", markersize=0.3)
 plt.show()
