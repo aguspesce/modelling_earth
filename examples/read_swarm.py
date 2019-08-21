f6c41e134116b6c71c58ae2dbf2e9bf7e94e998e
Agustina Pesce <pesce.agustina@gmail.com>
2019-08-21 19:09:11 -0300

    Add save_swarm funtion to an example

--- examples/read_swarm.py
+++ examples/read_swarm.py
@@ -8,10 +8,12 @@ import matplotlib.pyplot as plt
 # Get path to the MD3D output directory
 script_path = os.path.dirname(os.path.abspath(__file__))
 md3d_output_path = os.path.join(script_path, "run")
-output_path = os.path.join(script_path, "output")
+output_path = os.path.join(script_path, "_output")
 
 # Read the particles position
 swarm = me.read_swarm(path=md3d_output_path)
+# Save the particle position
+me.save_swarm(swarm, 'particle', output_path)
 
 # Take the particle position for time=0 linked to index 0
 positions = swarm["positions"]
@@ -26,5 +28,5 @@ data = me.read_md3d_data(md3d_output_path)
 # Plot the particle position over the temperature for step=0
 fig, ax = plt.subplots()
 me.plot_scalar_2d(data.temperature.sel(time=0, y=0), ax=ax)
-plt.scatter(positions.x, positions.z)
+plt.scatter(positions.x, positions.z, s=0.2, color='black', alpha=0.3)
 plt.show()
