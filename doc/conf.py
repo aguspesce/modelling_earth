f0f4eacdd7b52dfe54c3d85a04d6942e02c06a38
Santiago Soler <santiago.r.soler@gmail.com>
2019-08-23 11:29:07 -0300

    Change pandas url to https

--- doc/conf.py
+++ doc/conf.py
@@ -24,7 +24,7 @@ extensions = [
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
     "numpy": ("https://docs.scipy.org/doc/numpy/", None),
-    "pandas": ("http://pandas.pydata.org/pandas-docs/stable/", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
     "xarray": ("http://xarray.pydata.org/en/stable/", None),
     "matplotlib": ("https://matplotlib.org/", None),
 }
