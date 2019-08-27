1bf22d3b9ec2807b5362402d12190ae6edbb3b93
Agustina Pesce <pesce.agustina@gmail.com>
2019-08-27 14:50:36 -0300

    Improve the read_swarm example.

--- examples/read_swarm.py
+++ examples/read_swarm.py
@@ -12,17 +12,33 @@ md3d_output_path = os.path.join(script_path, "run")
 # Read the particles position
 swarm = me.read_md3d_swarm(md3d_output_path)
 
-# Take only the positions for the first step (step == 0)
-swarm = swarm.loc[0]
-
 # Reduce the number of particles to take only 2 particle per cell
 swarm = swarm[swarm.cc0 < 2]
 
-# Read the MD3D output files
-data = me.read_md3d_data(md3d_output_path)
+# Take only the positions for the first step (step == 0)
+swarm_0 = swarm.loc[10]
 
-# Plot the particle position over the temperature for step=0
+# Read the MD3D output files
+dataset = me.read_md3d_data(md3d_output_path)
+
+# Create filter to get only the y=0 profile of the dataset
+filter_profile = {"y": 0}
+
+## Plot the particle position over the temperature for step=0
 fig, ax = plt.subplots()
-me.plot_scalar_2d(data.temperature.sel(time=0, y=0), ax=ax)
-me.plot_swarm_2d(swarm, ax=ax)
+me.plot_scalar_2d(dataset.temperature.sel(time=0, **filter_profile), ax=ax)
+me.plot_swarm_2d(swarm_0, ax=ax)
 plt.show()
+
+# Plot all temperatures and particle position for every time and save the figures
+figs_dir = os.path.join(script_path, "_figures")
+if not os.path.isdir(figs_dir):
+    os.mkdir(figs_dir)
+me.save_plots_2d(
+    dataset.sel(**filter_profile),
+    figs_dir,
+    filename="temper_particle",
+    swarm=swarm,
+    scalar_to_plot="temperature",
+    plot_velocity=False,
+)
