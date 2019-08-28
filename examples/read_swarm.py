c1cab235b7c76adc92131290289ae303a3161a8a
Agustina Pesce <pesce.agustina@gmail.com>
2019-08-28 12:00:02 -0300

    Improve the example

--- examples/read_swarm.py
+++ examples/read_swarm.py
@@ -11,23 +11,18 @@ md3d_output_path = os.path.join(script_path, "run")
 
 # Read the particles position
 swarm = me.read_md3d_swarm(md3d_output_path)
-
 # Reduce the number of particles to take only 2 particle per cell
 swarm = swarm[swarm.cc0 < 2]
-
-# Take only the positions for the first step (step == 0)
-swarm_0 = swarm.loc[10]
 
 # Read the MD3D output files
 dataset = me.read_md3d_data(md3d_output_path)
 
-# Create filter to get only the y=0 profile of the dataset
-filter_profile = {"y": 0}
-
-## Plot the particle position over the temperature for step=0
+## Plot the particle position over the temperature for a fxed time step
+time = dataset.time.values[1]
+step = dataset.step.sel(time=time).values
 fig, ax = plt.subplots()
-me.plot_scalar_2d(dataset.temperature.sel(time=0, **filter_profile), ax=ax)
-me.plot_swarm_2d(swarm_0, ax=ax)
+me.plot_scalar_2d(dataset.temperature.sel(time=time, y=0), ax=ax)
+me.plot_swarm_2d(swarm.loc[step], ax=ax)
 plt.show()
 
 # Plot all temperatures and particle position for every time and save the figures
