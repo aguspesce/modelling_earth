8ae4caa07f9729d634d7f3324868773d1a3a2ee5
Santiago Soler <santiago.r.soler@gmail.com>
2019-09-18 11:11:12 -0300

    Make io/swarm.py able to read 2d data
    Add example for reading 2d swarm data. Rename examples/read_swarm.py to
    examples/read_swarm_3d.py.

--- examples/read_swarm_2d.py
+++ examples/read_swarm_2d.py
@@ -1,5 +1,5 @@
 """
-Read MD3D output files and plot temperature and position of the particle.
+Read and plot swarm positions from 2D data
 """
 import os
 import modelling_earth as me
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 # Get path to the MD3D output directory
 script_path = os.path.dirname(os.path.abspath(__file__))
-md3d_output_path = os.path.join(script_path, "run")
+md3d_output_path = os.path.join(script_path, "data", "data_2d")
 
 # Read the particles position
 swarm = me.read_md3d_swarm(md3d_output_path)
@@ -15,7 +15,7 @@ swarm = me.read_md3d_swarm(md3d_output_path)
 swarm = swarm[swarm.cc0 < 2]
 
 # Read the MD3D output files
-dataset = me.read_md3d_data(md3d_output_path)
+dataset = me.read_md3d_data(md3d_output_path, datasets=["temperature"])
 
 # Plot the particle position over the temperature for a fxed time step
 # Define the time and the step to plot
@@ -23,7 +23,7 @@ time = dataset.time.values[1]
 step = dataset.step.sel(time=time).values
 # Plot
 fig, ax = plt.subplots()
-me.plot_scalar_2d(dataset.temperature.sel(time=time, y=0), ax=ax)
+me.plot_scalar_2d(dataset.temperature.sel(time=time), ax=ax)
 me.plot_swarm_2d(swarm.loc[step], ax=ax)
 plt.show()
 
@@ -32,7 +32,7 @@ figs_dir = os.path.join(script_path, "_figures")
 if not os.path.isdir(figs_dir):
     os.mkdir(figs_dir)
 me.save_plots_2d(
-    dataset.sel(y=0),
+    dataset,
     figs_dir,
     filename="temper_particle",
     swarm=swarm,
