347ffa818122e64cf39dd66bce84f114a947fa78
Santiago Soler <santiago.r.soler@gmail.com>
2019-09-23 10:29:32 -0300

    Add function to save temperatures to an ASCII file
    Save temperatures distribution as an ASCII file ready to be ready by
    MD3D: following each axe in in crescent order, with the x changing
    faster than y, and y changing faster than z.

---
+++ modelling_earth/io/temperature.py
@@ -0,0 +1,45 @@
+"""
+Save temperature distributions to ASCII files to be read by MD3D
+"""
+import numpy as np
+
+HEADER = "T1 \n T2 \n T3 \n T4"
+
+
+def save_temperature(temperatures, fname):
+    """
+    Save temperatures grid as ASCII file ready to be read by MD3D
+
+    The temperatures grid values are saved on a single column, following each axe on
+    in crescent order, with the ``x`` indexes changing faster that the ``y``, and the
+    ``y`` faster than the ``z``.
+
+    Parameters
+    ----------
+    temperatures : :class:`xarray.DataArray`
+        Array containing a temperature distribution. Can be either 2D or 3D.
+    fname :
+       Filename of the output ASCII file.
+    """
+    # Check if temperatures is 2D or 3D
+    dimension = len(temperatures.dims)
+    if dimension == 3:
+        expected_dims = ("x", "y", "z")
+    elif dimension == 2:
+        expected_dims = ("x", "z")
+    else:
+        raise ValueError(
+            "Invalid temperatures array with dimension {}: ".format(dimension)
+            + "must be either 2 or 3."
+        )
+    # Check if temperature dims are in the correct order
+    if temperatures.dims != expected_dims:
+        raise ValueError(
+            "Invalid temperature dimensions '{}': ".format(temperatures.dims)
+            + "must be '{}' for {}D temperatures.".format(expected_dims, dimension)
+        )
+    # Ravel and save temperatures
+    # We will use order "F" on numpy.ravel in order to make the first index to change
+    # faster than the rest
+    # Will add a custom header required by MD3D
+    np.savetxt(fname, temperatures.values.ravel(order="F"), header=HEADER)
