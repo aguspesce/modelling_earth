77de75e231b414d11c1b4b209e953862798b197e
Agustina Pesce <pesce.agustina@gmail.com>
2019-09-23 16:55:28 -0300

    Add the save function to the example

--- examples/interfaces.py
+++ examples/interfaces.py
@@ -5,6 +5,13 @@ Create interfaces for a 2D temperature distrubution
 import modelling_earth as me
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+
+# Get path to the MD3D output directory
+script_path = os.path.dirname(os.path.abspath(__file__))
+md3d_input_path = os.path.join(script_path, "_input_data")
+if not os.path.isdir(md3d_input_path):
+    os.mkdir(md3d_input_path)
 
 # Define a region of study and the number of nodes per axes
 region = (0, 2000e3, -660e3, 0)
@@ -33,3 +40,17 @@ plt.gca().set_aspect("equal")
 interfaces.crust.plot()
 interfaces.lid.plot()
 plt.show()
+
+# Create layers data to create the layers file for MD3D
+layers = {
+    "viscosity_factor": [5, 100, 1000],
+    "density": [3300, 3000, 2900],
+    "radiogenic_heat": [0e-12, 0e-12, 1e-12],
+    "pre-factor": [1.393e-14, 1.393e-14, 2.4168e-15],
+    "exponential_factor": [3, 3, 3.5],
+    "activation_energy": [429e3, 429e3, 540e3],
+    "activation_volume": [15e-6, 15e-6, 25e-6],
+}
+
+# SAve the interfaces in a ASCII file
+me.save_interfaces(interfaces, layers, md3d_input_path)
