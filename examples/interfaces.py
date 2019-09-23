f217c74e41f39f3edd786abec7130a37221ea8ef
Agustina Pesce <pesce.agustina@gmail.com>
2019-09-23 12:46:26 -0300

    Complete the interface example

--- examples/interfaces.py
+++ examples/interfaces.py
@@ -1,20 +1,35 @@
+"""
+Create interfaces for a 2D temperature distrubution
+"""
+
 import modelling_earth as me
 import matplotlib.pyplot as plt
+import numpy as np
 
+# Define a region of study and the number of nodes per axes
 region = (0, 2000e3, -660e3, 0)
 shape = (251, 81)
 
+# Create an empty coordinates grid
 coordinates = me.grid_coordinates(region, shape)
 
+# Create a temperature distribution
+lid = me.initialize_array(coordinates).sel(z=coordinates["z"][0])
+lid += -300e3
+temperature = me.litho_astheno_temperatures(coordinates, lid_depth=lid)
+
+# Create the interfaces vertices
 vertices = [
-    [[0, -100e3], [500e3, -100e3], [1000e3, -150e3], [2000e3, -150e3]],
+    [[0, -300e3], [2000e3, -300e3]],
     [[0, -20e3], [500e3, -20e3], [1000e3, -45e3], [2000e3, -45e3]],
 ]
-names = ["lab", "crust"]
-
+names = ["lid", "crust"]
 interfaces = me.interfaces(vertices, coordinates, names)
-print(interfaces)
 
+# Plot temperature distribution
+temperature.plot.pcolormesh(x="x", y="z")
+plt.gca().set_aspect("equal")
+# Plot the interfaces
 interfaces.crust.plot()
-interfaces.lab.plot()
+interfaces.lid.plot()
 plt.show()
