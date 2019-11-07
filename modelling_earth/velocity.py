1510de35e6ba214d1b03c04d69a1fe6129d1f61c
Agustina Pesce <pesce.agustina@gmail.com>
2019-11-06 22:19:40 -0300

    Add a function to create a linear velocity distribution in the lateral boundaries.

--- modelling_earth/velocity.py
+++ modelling_earth/velocity.py
@@ -4,10 +4,10 @@ Functions to create 2D and 3D velocity distributions
 
 import numpy as np
 import xarray as xr
-from warnings import warn
+from .utilities import change_unit
 
 
-def linear_velocity(coordinates, z_start, velocity_bottom):
+def linear_velocity(coordinates, z_start, velocity_bottom=(0, 0)):
     """
     Create a linear velocity distribution in the lateral boundaries where the velocity
     is zero for z > z_start and for z_min < z < z_start assume a linear increase of
@@ -20,7 +20,7 @@ def linear_velocity(coordinates, z_start, velocity_bottom):
         created. Must be in meters and can be either 2D or 3D.
     z_start : float
         Depth value where the velocity condition changes in meters.
-    velocity_bottom : tuple
+    velocity_bottom :
         Velocity at bottom boundary (z_min) on the x and z axes.
 
     Returns
@@ -29,19 +29,23 @@ def linear_velocity(coordinates, z_start, velocity_bottom):
         Dataset containing the velocity distribution.
     """
     # Change the unit of the velocity from cm/years to m/s
-    velocity_bottom = velocity_bottom / (365 * 24 * 3600)
+    velocity_bottom_x, velocity_bottom_z = velocity_bottom[0], velocity_bottom[1]
+    velocity_bottom_x = change_unit(velocity_bottom_x)
+    velocity_bottom_z = change_unit(velocity_bottom_z)
     # Define the shape of the coordinates
     shape = (coordinates["x"].shape[0], coordinates["z"].shape[0])
     # Initialize the velocity dataset with zero values
     velocity = xr.Dataset(coords=coordinates)
-    velocity["velocity_x"], velocity["velocity_z"] = np.zeros(shape), np.zeros(shape)
+    velocity["velocity_x"] = (coordinates, np.zeros(shape))
+    velocity["velocity_z"] = (coordinates, np.zeros(shape))
     # Calculate the velocity assume a linear increase with depth
-    lin_velocity_x = velocity_bottom[0] * (velocity.z - z_start) / velocity.z[-1]
-    lin_velocity_z = velocity_bottom[1] * (velocity.z - z_start) / velocity.z[-1]
+    lin_velocity_x = velocity_bottom_x * (velocity.z - z_start) / velocity.z[0]
+    lin_velocity_z = velocity_bottom_z * (velocity.z - z_start) / velocity.z[0]
     # Add increasing velocity values on the first and last column
     condition = np.logical_and(
         velocity.z < z_start,
         np.logical_or(velocity.x == velocity.x[0], velocity.x == velocity.x[-1]),
     )
-    velocity.x.where(~condition, lin_velocity_x)
-    velocity.z.where(~condition, lin_velocity_z)
+    velocity["velocity_x"] = velocity.velocity_x.where(~condition, lin_velocity_x)
+    velocity["velocity_z"] = velocity.velocity_z.where(~condition, lin_velocity_z)
+    return velocity
