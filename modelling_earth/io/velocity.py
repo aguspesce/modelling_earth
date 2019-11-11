bb0034118167f1d2d313cf97089cb90e423bac2d
Agustina Pesce <pesce.agustina@gmail.com>
2019-11-11 12:26:25 -0300

    Improve the velocity save function

--- modelling_earth/io/velocity.py
+++ modelling_earth/io/velocity.py
@@ -27,26 +27,18 @@ def save_velocity(velocity, path, fname=FNAME):
        Filename of the output ASCII file. Default to ``veloc_0_3D.txt``.
     """
     dimension = len(velocity.dims)
-    # Initialize the velocity to save
     shape = velocity.velocity_x.shape
+    size = np.array(shape).prod()
     if dimension == 3:
-        velocity_to_save = np.zeros((dimension, shape[0] * shape[1] * shape[2]))
-        # Ravel the velocity components and save them to a single file
-        # We will use order "F" on numpy.ravel in order to make the first index to
-        # change faster than the rest
-        velocity_to_save[0, :] = velocity.velocity_x.values.ravel(order="F")
-        velocity_to_save[1, :] = velocity.velocity_y.values.ravel(order="F")
-        velocity_to_save[2, :] = velocity.velocity_z.values.ravel(order="F")
-        velocity_to_save = np.reshape(velocity_to_save.T, (np.size(velocity_to_save)))
-        # Will add a custom header required by MD3D/MD2D
-        return np.savetxt(os.path.join(path, fname), velocity_to_save, header=HEADER)
-    else:
-        velocity_to_save = np.zeros((dimension, shape[0] * shape[1]))
-        # Revel the velocity component and put them in only file
-        # We will use order "F" on numpy.ravel in order to make the first index to
-        # change faster than the rest
-        velocity_to_save[0, :] = velocity.velocity_x.values.ravel(order="F")
-        velocity_to_save[1, :] = velocity.velocity_z.values.ravel(order="F")
-        velocity_to_save = np.reshape(velocity_to_save.T, (np.size(velocity_to_save)))
-        # Will add a custom header required by MD3D/MD2D
-        return np.savetxt(os.path.join(path, fname), velocity_to_save, header=HEADER)
+        components = (velocity.velocity_x, velocity.velocity_y, velocity.velocity_z)
+    elif dimension == 2:
+        components = (velocity.velocity_x, velocity.velocity_z)
+    velocity_to_save = np.zeros((dimension, size))
+    # Ravel the velocity components and save them to a single file
+    # We will use order "F" on numpy.ravel in order to make the first index to change
+    # faster than the rest
+    for i, component in enumerate(components):
+        velocity_to_save[i, :] = component.values.ravel(order="F")
+    velocity_to_save = np.reshape(velocity_to_save.T, (np.size(velocity_to_save)))
+    # Will add a custom header required by MD3D/MD2D
+    return np.savetxt(os.path.join(path, fname), velocity_to_save, header=HEADER)
