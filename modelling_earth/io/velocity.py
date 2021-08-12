7ab4bb99d635f6526de3c4e8bffa8293876ecb66
Agustina Pesce <pesce.agustina@gmail.com>
2021-08-12 16:12:48 -0300

    Fix an error on how the function save the velocity values

--- modelling_earth/io/velocity.py
+++ modelling_earth/io/velocity.py
@@ -13,14 +13,12 @@ def save_velocity(velocity, path, fname=FNAME):
     Save velocity grid as ASCII file ready to be read by MANDYOC
 
     The velocity grid values are saved on three column, following each axe on
-    in crescent order, with the ``x`` indexes changing faster that the ``y``, and the
-    ``y`` faster than the ``z``.
+    in crescent order, with the ``x`` indexes changing faster that the ``y``.
 
     Parameters
     ----------
     velocity : :class:`xarray.Dataset`
-        Dataset containing the three velocity component distribution. Can be either 2D
-        or 3D.
+        Dataset containing the three velocity component distribution.
     path : str
         Path to save the velocity file.
     fname : str (optional)
@@ -29,16 +27,15 @@ def save_velocity(velocity, path, fname=FNAME):
     dimension = len(velocity.dims)
     shape = velocity.velocity_x.shape
     size = np.array(shape).prod()
-    if dimension == 3:
-        components = (velocity.velocity_x, velocity.velocity_y, velocity.velocity_z)
-    elif dimension == 2:
-        components = (velocity.velocity_x, velocity.velocity_z)
+    
+    components = (velocity.velocity_x.T, velocity.velocity_z)
     velocity_to_save = np.zeros((dimension, size))
-    # Ravel the velocity components and save them to a single file
-    # We will use order "F" on numpy.ravel in order to make the first index to change
-    # faster than the rest
+    
     for i, component in enumerate(components):
-        velocity_to_save[i, :] = component.values.ravel(order="F")
-    velocity_to_save = np.reshape(velocity_to_save.T, (np.size(velocity_to_save)))
+        velocity_to_save[i, :] = np.reshape(component.values, size)
+    velocity_to_save = np.reshape(
+        velocity_to_save.T, 
+        (np.size(velocity_to_save))
+        )
     # Will add a custom header required by MANDYOC
     return np.savetxt(os.path.join(path, fname), velocity_to_save, header=HEADER)
